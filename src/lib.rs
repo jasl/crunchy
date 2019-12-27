45bea7846d9f8d2707ed14d486b9cb3a0c04c51c
jasl <jasl9187@hotmail.com>
2019-12-28 00:21:29 +0800

    port to sgx

--- src/lib.rs
+++ src/lib.rs
@@ -32,5 +32,11 @@
 //! * `limit_2048`
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
 
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
